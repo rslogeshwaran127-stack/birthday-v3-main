24da435de83e1078ae4254096fd27d0e35d22f39
rslogeshwaran127-stack <rslogeshwaran127@gmail.com>
2025-11-24 20:24:28 +0530

    commit

--- src/components/screens/PhotosScreen.jsx
+++ src/components/screens/PhotosScreen.jsx
@@ -13,10 +13,10 @@ export default function PhotosScreen({ onNext }) {
   const swiperRef = useRef(null)
 
   const photos = [
-    "/images/baby1.jpg",
-    "/images/baby2.jpg",
-    "/images/baby3.jpg",
-    "/images/baby4.jpg",   
+    "/images/lathi.jpg",
+    "/images/lathi1.jpg",
+    "/images/lathi2.jpg",
+    "/images/lathi3.jpg",   
   ]
 
   return (
